c48acec2958e81653df188c87bbdb90af96b17cb
sikarwar2010 <sikarwar2010@gmail.com>
2026-05-07 13:15:53 +0530

    feat:auth login & sign up

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,16 @@
+import { SignUpForm } from '@/components/auth/signup/signup-form';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function SignUpPage() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link href="" className="flex items-center gap-2 self-center font-medium">
+          <Image src="/sdv.jpeg" alt="Logo" width={250} height={250} className="rounded-md" />
+        </Link>
+        <SignUpForm />
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <div>Welcome to the SDV Survey App</div>;
 }
